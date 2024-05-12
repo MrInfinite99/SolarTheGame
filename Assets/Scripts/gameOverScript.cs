100b3aded913ad14652a7495703c483822765b39
MrInfinite99 <karan97344@gmail.com>
2024-05-12 18:25:38 +0530

    add project

---
+++ Assets/Scripts/gameOverScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameOverScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image gameOverImage;
+    public GameObject canvasGameOver;
+   // public playerEnemyCollision playerEnemyCollision;
+    void Start()
+    {
+     //   gameOverImage.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (playerEnemyCollision.gameOver)
+       // {
+        //    gameOverImage.enabled = true;
+       //     canvasGameOver.SetActive(true);
+        
+   // }
+    }
+}
